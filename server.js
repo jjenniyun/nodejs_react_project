c8f216efa4e2c77561dc8a06a009e7557655f45d
jjenniyun <hjkjj1847@gmail.com>
2023-03-27 12:42:13 +0900

    nodejs_react 수정(23.03.27)

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const app = express();
 app.listen(8080, function () {
     console.log('listening on 8080')
 });
+
+app.use(expree.json());
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'react_nodejs/build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'react_nodejs/build/index.html'));
+})
+
+app.get('/product', function (req, res) {
+    res.json({ name: 'black shoes' })
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'react_nodejs/build/index.html'));
+})
